a7257d70760befc92dca537a8c78eb17599d30a4
HouStanley456 <roy.ron@hotmail.com>
2022-02-23 20:14:07 +0800

    first commit

---
+++ src/pages/Help.vue
@@ -0,0 +1,15 @@
+<template>
+  <q-page class="q-pa-lg">
+    <h5 class="q-mt-none">Help</h5>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit eaque voluptatem consectetur commodi sit sint nostrum, eum doloribus veniam nihil deserunt vero ea cumque explicabo quas est mollitia nisi aut. </p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis sit quae suscipit magni, sunt quia nobis iste. Placeat voluptatibus laborum ea sint a, obcaecati repudiandae facilis. Nemo possimus consequatur sequi.</p>
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+
+})
+</script>
